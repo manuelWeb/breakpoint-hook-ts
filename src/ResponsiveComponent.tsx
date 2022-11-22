4a0874e1b88366054d1f5842f753364703c64fe9
Manu2015 <devries.webdesigner@gmail.com>
2022-11-22 19:22:13 +0100

    **feat** useViewport return breakpoint string

--- src/ResponsiveComponent.tsx
+++ src/ResponsiveComponent.tsx
@@ -2,23 +2,11 @@ import React from 'react';
 import useViewport from './useViewport';
 
 const ResponsiveComponent = () => {
-  const { width } = useViewport();
-  const breakpoint = 620
+  const { currBp } = useViewport();
 
   return (
-    width < breakpoint ? <Mobile /> : <Desktop />
+    <h1>{currBp}</h1>
   );
 };
 
 export default ResponsiveComponent;
-
-function Mobile() {
-  return (
-    <h1>Je suis en dessous de 620</h1>
-  )
-}
-function Desktop() {
-  return (
-    <h1>Je suis &gt;620 Desktop</h1>
-  )
-}
--- src/useViewport.tsx
+++ src/useViewport.tsx
@@ -1,18 +1,44 @@
 import { useEffect, useState } from 'react';
-
+const breakpoints: any = {
+  xs: 320,
+  sm: 600,
+  md: 900,
+  lg: 1000,
+  xl: 1100,
+}
+function isGreaterThan(window: number, bp: number): boolean {
+  return window <= bp
+}
 const useViewport = () => {
   const [width, setWidth] = useState(window.innerWidth)
+  const [currBp, setCurrBp] = useState<null | string>(null)
 
   useEffect(() => {
     const handleWidowResize = () => setWidth(window.innerWidth)
     window.addEventListener('resize', handleWidowResize)
-
     // rm listener componentWillUnmount
     return (() => { window.removeEventListener('resize', handleWidowResize) })
-
+  }, [width])
+  /**
+   * transform window.innerWidth to breakpoints string
+   */
+  useEffect(() => {
+    if (isGreaterThan(width, breakpoints.xl)) {
+      setCurrBp('xl')
+    } else if (isGreaterThan(width, breakpoints.lg)) {
+      setCurrBp('lg')
+    } else if (isGreaterThan(width, breakpoints.md)) {
+      setCurrBp('md')
+    } else if (isGreaterThan(width, breakpoints.sm)) {
+      setCurrBp('sm')
+    } else if (isGreaterThan(width, breakpoints.xs)) {
+      setCurrBp('xs')
+    } else {
+      setCurrBp('xxlarge')
+    }
   }, [width])
 
-  return { width }
+  return { currBp }
 
 };
 
